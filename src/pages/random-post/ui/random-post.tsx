6db978527b7909c1c38cdb545659dbdcbd1d37fb
Anton Panza <56302827+antonpanza@users.noreply.github.com>
2025-03-12 02:44:36 +0200

    feat: implemented API request for random post and nav

---
+++ src/pages/random-post/ui/random-post.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { postsApi } from "@/shared/api/posts-api";
+import styles from '@/styles/common.module.scss';
+import { Button } from "@/shared/ui/button/Button";
+
+
+export function RandomPost() {
+  const [postTitle, setPostTitle] = useState<string | null>(null);
+
+  const fetchRandomPost = async () => {
+    try {
+      const post = await postsApi.getRandomPost();
+      setPostTitle(post.title);
+    } catch (error) {
+      console.error("Ошибка загрузки поста:", error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Рандомный пост</h1>
+      <Button label="Получить" onClick={fetchRandomPost} />
+      {postTitle && <h2>{postTitle}</h2>}
+    </div>
+  );
+}
---
+++ src/widgets/navigation/ui/navigation.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import { routes } from "@/shared/config/routes";
+import styles from "./navigation.module.scss";
+
+export function Navigation() {
+  return (
+    <nav className={styles.navContainer}>
+      <Link to={routes.getHome()} className={styles.navLink}>Главная</Link>
+      <Link to={routes.getRandomPost()} className={styles.navLink}>Рандомный пост</Link>
+    </nav>
+  );
+}
